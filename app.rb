55be824d35c5d313e26af01805395015e52306b3
Burak Kocak <burakkocak884@ghotmail.com>
2019-04-04 13:36:18 -0400

    error on fist method

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+	get '/' do 
+		erb :user_input
+	end
+	post '/piglatinize' do
+		@word = params["user_type"]
+
+		new_word = PigLatinizer.new
+		@pl = new_word.piglatinize(@word)
+
+		erb :pig_latin_display
+
+ end
+
+
+
+
 end
